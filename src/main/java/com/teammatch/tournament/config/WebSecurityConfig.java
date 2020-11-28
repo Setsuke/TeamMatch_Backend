8d81a1edb88ea429a6e6e0a03ae1ab8bab60c572
DiegoJohnsonL <diegojohnson46@gmail.com>
2020-11-27 23:55:41 -0500

    q-q

--- src/main/java/com/teammatch/tournament/config/WebSecurityConfig.java
+++ src/main/java/com/teammatch/tournament/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/*").permitAll()
+                .antMatchers("/api/auth/*", "/api/players/*").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint).and()
