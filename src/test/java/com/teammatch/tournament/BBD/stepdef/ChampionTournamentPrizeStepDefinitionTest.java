aaae3b219ad089c61ebae21000586cd8334ccb14
Sergio Alva <sergio_alva99@hotmail.com>
2021-04-24 15:32:56 -0500

    BDD Testing HU7,11,15 update

--- src/test/java/com/teammatch/tournament/BBD/stepdef/ChampionTournamentPrizeStepDefinitionTest.java
+++ src/test/java/com/teammatch/tournament/BBD/stepdef/ChampionTournamentPrizeStepDefinitionTest.java
@@ -5,20 +5,29 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ChampionTournamentPrizeStepDefinitionTest {
-    @Given("the user wants to know the <initial prize> of the tournament")
-    public void theUserWantsToKnowTheInitialPrizeOfTheTournament() {
+
+    private int initial_prize;
+    private int actual_prize;
+
+    @Given("the user wants to know the {int} of the tournament")
+    public void theUserWantsToKnowTheInitialPrizeOfTheTournament(int initial_prize) {
+        this.initial_prize = initial_prize;
     }
 
-    @And("the system added a new <player enrollment> to the tournament")
-    public void theSystemAddedANewPlayerEnrollmentToTheTournament() {
+    @And("the system added a new {int} to the tournament")
+    public void theSystemAddedANewPlayerEnrollmentToTheTournament(int added_enrollment) {
+        this.actual_prize = this.initial_prize+added_enrollment;
     }
 
     @When("I refresh the view")
     public void iRefreshTheView() {
     }
 
-    @Then("the user will see the <actual prize> of the tournament successfully")
-    public void theUserWillSeeTheActualPrizeOfTheTournamentSuccessfully() {
+    @Then("the user will see the {int} of the tournament successfully")
+    public void theUserWillSeeTheActualPrizeOfTheTournamentSuccessfully(int actual_prize) {
+        assertEquals(this.actual_prize,actual_prize);
     }
 }
